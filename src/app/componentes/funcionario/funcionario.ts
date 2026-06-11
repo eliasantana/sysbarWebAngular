54403fca46e78cb1db49eef8811899adb6f77d4b
eliasantana <eliasantanasilva@gmail.com>
2026-06-11 20:51:40 -0300

    ADICIONANDO FORMULÁRIO FUNCIONÁRIO

--- src/app/componentes/funcionario/funcionario.ts
+++ src/app/componentes/funcionario/funcionario.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatFormFieldControl, MatFormFieldModule } from "@angular/material/form-field";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskDirective,provideNgxMask } from "ngx-mask";
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-funcionario',
-  imports: [RouterLink],
+  imports: [MatFormFieldModule, FormsModule, ReactiveFormsModule, MatInputModule, NgxMaskDirective], 
+  providers:[provideNgxMask(), provideNativeDateAdapter()],
   templateUrl: './funcionario.html',
   styleUrl: './funcionario.css',
 })
-export class Funcionario {}
+export class Funcionario {
+
+   formularioFuncionario = new FormGroup({
+      cdFuncionario:new FormControl(''),
+      nome:new FormControl(''),
+      caminhoImagem:new FormControl(''),
+      telefone:new FormControl(''),
+      login:new FormControl(''),
+      senha:new FormControl(''),
+      snAtivo:new FormControl(''),
+      nrRg:new FormControl(''),
+      nrCpf:new FormControl(''),
+      nrCnh:new FormControl(''),
+      observacao :new FormControl(''),
+      dtvalidadeCNH:new FormControl(''),
+      dtDesligamento:new FormControl(''),
+      dtAdmissao:new FormControl(''),
+      dtNascimento:new FormControl(''),
+      dtInclusao:new FormControl('')
+  });
+
+}
